5e348c3dbbd06d308e77048697aef5b923a9eb72
noam9472-gif <noam9472@gmail.com>
2025-12-09 12:05:45 +0200

    finish SelectCollector

--- Movies.API/Controllers/SelectController.cs
+++ Movies.API/Controllers/SelectController.cs
@@ -5,49 +5,61 @@ using ViewModel;
 
 namespace Movies.API.Controllers
 {
+    [Route("api/[controller]/[action]")]
+    [ApiController]
     public class SelectController : Controller
     {
+        
         // GET: SelectController
         [HttpGet]
-        [ActionName("MoviesSelector")]
+        [ActionName("VideoSelector")]
         public VideoList SelectAllVideos()
         {
             VideoDB db = new VideoDB();
             VideoList videos = db.SelectAll();
             return videos;
         }
+        [HttpGet]
+        [ActionName("UserSelector")]
         public UserList SelectAllUsers()
         {
             UserDB db = new UserDB();
             UserList videos = db.SelectAll();
             return videos;
         }
+        [HttpGet]
+        [ActionName("GenreSelector")]
         public GenreList SelectAllGenres()
         {
             GenreDB db = new GenreDB();
             GenreList genres = db.SelectAll();
             return genres;
         }
+        [HttpGet]
+        [ActionName("VideoReviewSelector")]
         public VideoReviewList SelectAllVideoReviews()
         {
             VideoReviewDB db = new VideoReviewDB();
             VideoReviewList videoReviews = db.SelectAll();
             return videoReviews;
         }
+        [HttpGet]
+        [ActionName("AgeOfVideoSelector")]
         public AgeOfVideoList SelectAllAgeOfVideos()
         {
             AgeOfVideosDB db = new AgeOfVideosDB();
             AgeOfVideoList ageOfVideos = db.SelectAll();
             return ageOfVideos;
         }
+        [HttpGet]
+        [ActionName("UserPremiumSelector")]
         public UserPremiumList SelectAllUserPremiums()
         {
             UserPremiumDB db = new UserPremiumDB();
             UserPremiumList movies = db.SelectAll();
             return movies;
         }
-        [HttpGet]
-        [ActionName("MovieSelector")]
+        
         public ActionResult Index()
         {
             return View();
