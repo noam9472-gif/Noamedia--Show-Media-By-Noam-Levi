b66ada003f56e310847e19b67c22c2ada06f7e3f
נעם לוי <noam9472@gmail.com>
2025-12-17 10:04:37 +0200

    complete Insert controller

--- Movies.API/Controllers/InsertController.cs
+++ Movies.API/Controllers/InsertController.cs
@@ -1,15 +1,68 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Model;
+using ViewModel;
 
 namespace Movies.API.Controllers
 {
     public class InsertController : Controller
     {
         // GET: InsertController
-        public ActionResult Index()
+        [HttpPost]
+        [ActionName("VideoInserter")]
+        public int InsertVideo([FromBody] Video video)
         {
-            return View();
+            VideoDB vdb = new VideoDB();
+            vdb.Insert(video);
+            int x = vdb.SaveChanges();
+            return x;
         }
+        [HttpPost]
+        [ActionName("VideoReviewInserter")]
+        public int InsertVideoReview([FromBody] VideoReview videoReview)
+        {
+            VideoReviewDB vrdb = new VideoReviewDB();
+            vrdb.Insert(videoReview);
+            int x = vrdb.SaveChanges();
+            return x;
+        }
+        [HttpPost]
+        [ActionName("UserInserter")]
+        public int InsertUser([FromBody] User user)
+        {
+            UserDB udb = new UserDB();
+            udb.Insert(user);
+            int x = udb.SaveChanges();
+            return x;
+        }
+        [HttpPost]
+        [ActionName("UserPremiumInserter")]
+        public int InsertUserPremium([FromBody] UserPremium userPremium)
+        {
+            UserPremiumDB updb = new UserPremiumDB();
+            updb.Insert(userPremium);
+            int x = updb.SaveChanges();
+            return x;
+        }
+        [HttpPost]
+        [ActionName("GenreInserter")]
+        public int InsertGenre([FromBody] Genre genre)
+        {
+            GenreDB gdb = new GenreDB();
+            gdb.Insert(genre);
+            int x = gdb.SaveChanges();
+            return x;
+        }
+        [HttpPost]
+        [ActionName("AgeOfVideoInserter")]
+        public int InsertAgeOfVideo([FromBody] AgeOfVideos ageOfVideo)
+        {
+            AgeOfVideoDB aovdb = new AgeOfVideoDB();
+            aovdb.Insert(ageOfVideo);
+            int x = aovdb.SaveChanges();
+            return x;
+        }
+
 
         // GET: InsertController/Details/5
         public ActionResult Details(int id)
